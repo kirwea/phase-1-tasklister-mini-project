0f01a790e2aebb0d1ee6550bac437350b312c120
dennistaiti <dennistaiti7@gmail.com>
2022-12-06 16:33:09 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  form.addEventListener("submit", handleTodo);
 });
+const form = document.getElementById("create-task-form");
+
+function handleTodo(e) {
+  e.preventDefault();
+
+let newTask = e.target[0].value;
+
+let todo = document.getElementById("tasks");
+let li = document.createElement("li");
+let deleteBtn = document.createElement("button");
+deleteBtn.textContent = "Delete;";
+deleteBtn.classList.add("delete-btn");
+li.textContent = newTask;
+li.append(deleteBtn);
+
+todo.appendChild(li);
+
+deleteBtn.addEventListener("click",(e)=>{
+deleteBtn.parentNode.remove()
+
+})
+
+}
